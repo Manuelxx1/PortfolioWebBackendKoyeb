21eb4af2eaea34fedd5755b349cc470e022a87e3
Luis Manuel Alegre <106556022+Manuelxx1@users.noreply.github.com>
2025-08-19 03:46:20 -0300

    Update NoticiaRepository.java

--- src/main/java/com/abml/jpa/hibernate/repository/NoticiaRepository.java
+++ src/main/java/com/abml/jpa/hibernate/repository/NoticiaRepository.java
@@ -1,3 +1,34 @@
+package com.abml.jpa.hibernate.repository;
+
+/**
+ *
+ * @author Flash
+ */
+
+import com.abml.jpa.hibernate.model.Noticia;
+import java.util.List;
+import java.util.Optional;
+//class JpaRepository (que maneja repositorios JPA
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
+
+
+
+/**
+ *
+ * @author Flash
+ */
+//hacemos un mapping con @Repository
+@Repository 
+@Component
+
+
+
+
 public interface NoticiaRepository extends JpaRepository<Noticia, Long> {
     List<Noticia> findByInformacionContainingIgnoreCase(String keyword);
 }
