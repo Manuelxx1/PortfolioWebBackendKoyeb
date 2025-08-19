7589fb7439740970df5886914a6faa8f25ccbbfa
Luis Manuel Alegre <106556022+Manuelxx1@users.noreply.github.com>
2025-08-19 05:48:59 -0300

    Create MainApplication.java
    Class creada para organizar el código

---
+++ src/main/java/com/abml/jpa/hibernate/MainApplication.java
@@ -0,0 +1,17 @@
+@SpringBootApplication
+//para crear las tables en nuestra base de datos
+//es fundamental las siguientes anotations
+@EnableJpaRepositories (basePackages = "com.abml.jpa.hibernate.repository") //Escanea el repositorio bajo el paquete especificado
+
+@EntityScan (basePackages = "com.abml.jpa.hibernate.model")//Escanea las entidades bajo el paquete especificado
+
+@ComponentScan (basePackages = "com.abml.jpa.hibernate.service", "com.abml.jpa.hibernate.model", "com.ejercicioabml.abmlcontroller.NoticiaController",
+"com.abml.jpa.hibernate.repository" ) // Especifica el paquete que se va a escanear, de lo contrario, solo el paquete donde se encuentra esta clase
+
+
+public class AbmlcontrollerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AbmlcontrollerApplication.class, args);
+	}
+        
