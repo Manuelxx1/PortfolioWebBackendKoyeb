f2c276920f201c120b42b9934a88a178aec3458a
Luis Manuel Alegre <106556022+Manuelxx1@users.noreply.github.com>
2025-08-20 05:08:25 -0300

    Update AbmlApplication.java

--- src/main/java/com/abml/AbmlApplication.java
+++ src/main/java/com/abml/AbmlApplication.java
@@ -1,27 +1,23 @@
 package com.abml;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-
-
 @SpringBootApplication
-//para crear las tables en nuestra base de datos
-//es fundamental las siguientes anotations
-@EnableJpaRepositories (basePackages = "com.abml.jpa.hibernate.repository") //Escanea el repositorio bajo el paquete especificado
-
-@EntityScan (basePackages = "com.abml.jpa.hibernate.model")//Escanea las entidades bajo el paquete especificado
-
-@ComponentScan (basePackages = "com.abml.jpa.hibernate.service", "com.abml.jpa.hibernate.model", "com.ejercicioabml.abmlcontroller.NoticiaController",
-"com.abml.jpa.hibernate.repository" ) // Especifica el paquete que se va a escanear, de lo contrario, solo el paquete donde se encuentra esta clase
-
-
+@EnableJpaRepositories(basePackages = "com.abml.jpa.hibernate.repository")
+@EntityScan(basePackages = "com.abml.jpa.hibernate.model")
+@ComponentScan(basePackages = {
+    "com.abml.jpa.hibernate.service",
+    "com.abml.jpa.hibernate.model",
+    "com.ejercicioabml.abmlcontroller",
+    "com.abml.jpa.hibernate.repository"
+})
 public class AbmlApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AbmlApplication.class, args);
-	}
-        
-				}
+    public static void main(String[] args) {
+        SpringApplication.run(AbmlApplication.class, args);
+    }
+}
