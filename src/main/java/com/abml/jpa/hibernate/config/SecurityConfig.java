74145a7c6607d87ca4c295874caf8afb5a5e1f8c
Luis Manuel Alegre <106556022+Manuelxx1@users.noreply.github.com>
2025-09-30 06:35:26 -0300

    Update SecurityConfig.java

--- src/main/java/com/abml/jpa/hibernate/config/SecurityConfig.java
+++ src/main/java/com/abml/jpa/hibernate/config/SecurityConfig.java
@@ -1,3 +1,24 @@
+
+package com.abml.jpa.hibernate.config;
+
+import com.abml.jpa.hibernate.model.Persona;
+import com.abml.jpa.hibernate.security.JwtFilter;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+
+
+
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
