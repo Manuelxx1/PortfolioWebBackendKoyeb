c36d114cc498b9d5072f2aea25b45d5d378c8a6b
Luis Manuel Alegre <106556022+Manuelxx1@users.noreply.github.com>
2025-10-01 05:50:58 -0300

    Update AbmlcontrollerApplication.java

--- src/main/java/com/ejercicioabml/abmlcontroller/AbmlcontrollerApplication.java
+++ src/main/java/com/ejercicioabml/abmlcontroller/AbmlcontrollerApplication.java
@@ -10,9 +10,19 @@ import org.springframework.beans.factory.annotation.Value;
 
 //import org.springframework.beans.factory.annotation.Autowired;
 //import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
+//import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.web.bind.annotation.*;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.http.ResponseEntity;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationConfiguration;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+
 
 //import java.util.Map;
 import com.abml.jpa.hibernate.service.PersonaService;
