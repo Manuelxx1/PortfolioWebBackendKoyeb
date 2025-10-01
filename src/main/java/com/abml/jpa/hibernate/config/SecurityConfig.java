632e537ec2ba09c7d016d60431771cd28e86c395
Luis Manuel Alegre <106556022+Manuelxx1@users.noreply.github.com>
2025-10-01 03:38:07 -0300

    Update SecurityConfig.java

--- src/main/java/com/abml/jpa/hibernate/config/SecurityConfig.java
+++ src/main/java/com/abml/jpa/hibernate/config/SecurityConfig.java
@@ -1,29 +1,29 @@
-
 package com.abml.jpa.hibernate.config;
 
-import com.abml.jpa.hibernate.model.Persona;
 import com.abml.jpa.hibernate.security.JwtFilter;
-import org.springframework.security.authentication.AuthenticationConfiguration;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-
-
-
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
-  @Autowired private JwtFilter jwtFilter;
+
+  @Autowired
+  private JwtFilter jwtFilter;
 
   @Bean
   public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -41,5 +41,10 @@ public class SecurityConfig {
   @Bean
   public PasswordEncoder passwordEncoder() {
     return new BCryptPasswordEncoder();
+  }
+
+  @Bean
+  public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+    return config.getAuthenticationManager();
   }
 }
