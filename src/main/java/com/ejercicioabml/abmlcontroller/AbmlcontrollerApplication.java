48c12d62c66c6cdff311bd5ed6eabe095ad5744d
Luis Manuel Alegre <106556022+Manuelxx1@users.noreply.github.com>
2025-10-01 07:42:18 -0300

    Update AbmlcontrollerApplication.java

--- src/main/java/com/ejercicioabml/abmlcontroller/AbmlcontrollerApplication.java
+++ src/main/java/com/ejercicioabml/abmlcontroller/AbmlcontrollerApplication.java
@@ -13,7 +13,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.AuthenticationConfiguration;
+//import org.springframework.security.authentication.AuthenticationConfiguration;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 
 import org.springframework.web.bind.annotation.*;
