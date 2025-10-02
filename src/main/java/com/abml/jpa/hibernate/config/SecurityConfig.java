73687f0a6dfbf09f89e6ee6e0572e58a43dc6e3e
Luis Manuel Alegre <106556022+Manuelxx1@users.noreply.github.com>
2025-10-02 03:02:06 -0300

    Update SecurityConfig.java

--- src/main/java/com/abml/jpa/hibernate/config/SecurityConfig.java
+++ src/main/java/com/abml/jpa/hibernate/config/SecurityConfig.java
@@ -7,7 +7,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import org.springframework.security.authentication.AuthenticationManager;
-//import org.springframework.security.authentication.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -25,10 +24,6 @@ public class SecurityConfig {
 
   @Autowired
   private JwtFilter jwtFilter;
-
-  @Autowired
-private AuthenticationManager authenticationManager;
-
 
   @Bean
   public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -51,5 +46,8 @@ private AuthenticationManager authenticationManager;
     return new BCryptPasswordEncoder();
   }
 
-  
+  @Bean
+  public AuthenticationManager authenticationManager(HttpSecurity http) throws Exception {
+    return http.getSharedObject(AuthenticationManager.class);
+  }
 }
