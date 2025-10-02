40583a7471deae6bef0a1ddeaeecbb7dd9505b8b
Luis Manuel Alegre <106556022+Manuelxx1@users.noreply.github.com>
2025-10-02 04:05:00 -0300

    Update JwtFilter.java

--- src/main/java/com/ejercicioabml/abmlcontroller/security/JwtFilter.java
+++ src/main/java/com/ejercicioabml/abmlcontroller/security/JwtFilter.java
@@ -3,7 +3,8 @@ package com.ejercicioabml.abmlcontroller.security;
 
 import com.abml.jpa.hibernate.model.Persona;
 import com.abml.jpa.hibernate.service.PersonaService;
-import com.abml.jpa.hibernate.security.JwtUtil;
+
+import com.ejercicioabml.abmlcontroller.security.JwtUtil;
 
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
