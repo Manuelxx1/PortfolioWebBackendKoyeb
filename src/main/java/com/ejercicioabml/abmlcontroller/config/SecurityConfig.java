5a6eb2b04e2cba92a8926560f79b488d05278fbe
Luis Manuel Alegre <106556022+Manuelxx1@users.noreply.github.com>
2025-10-02 04:11:50 -0300

    Update SecurityConfig.java

--- src/main/java/com/ejercicioabml/abmlcontroller/config/SecurityConfig.java
+++ src/main/java/com/ejercicioabml/abmlcontroller/config/SecurityConfig.java
@@ -1,7 +1,7 @@
 package com.ejercicioabml.abmlcontroller.config;
 
 
-import com.abml.jpa.hibernate.security.JwtFilter;
+import com.ejercicioabml.abmlcontroller.security.JwtFilter;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
