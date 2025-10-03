1defc0d16e1e39d1e4e259cf2761688bc4b36346
Luis Manuel Alegre <106556022+Manuelxx1@users.noreply.github.com>
2025-10-03 01:55:42 -0300

    Update SecurityConfig.java

--- src/main/java/com/ejercicioabml/abmlcontroller/config/SecurityConfig.java
+++ src/main/java/com/ejercicioabml/abmlcontroller/config/SecurityConfig.java
@@ -8,10 +8,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationConfiguration;
 
 
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+//import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.AuthenticationConfiguration;
+//import org.springframework.security.authentication.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
