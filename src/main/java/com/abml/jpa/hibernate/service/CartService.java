44608d35298a537dbd220b9a6ca846bd44364a49
Luis Manuel Alegre <106556022+Manuelxx1@users.noreply.github.com>
2025-11-11 05:05:37 -0300

    Update CartService.java

--- src/main/java/com/abml/jpa/hibernate/service/CartService.java
+++ src/main/java/com/abml/jpa/hibernate/service/CartService.java
@@ -1,3 +1,8 @@
+import org.springframework.data.jpa.repository.ProductRepository;
+import org.springframework.data.jpa.repository.CartItemRepository;
+
+import com.abml.jpa.hibernate.model.User;
+
 @Service
 public class CartService {
 
