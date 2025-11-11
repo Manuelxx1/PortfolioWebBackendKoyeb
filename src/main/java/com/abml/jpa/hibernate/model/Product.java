c0ef7efe54d51ac0bff5fa58e7d8674fddb692e6
Luis Manuel Alegre <106556022+Manuelxx1@users.noreply.github.com>
2025-11-11 06:37:06 -0300

    Update Product.java

--- src/main/java/com/abml/jpa/hibernate/model/Product.java
+++ src/main/java/com/abml/jpa/hibernate/model/Product.java
@@ -11,6 +11,10 @@ import jakarta.persistence.ManyToOne;
 import jakarta.persistence.JoinColumn;
 
 
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
+
 @Entity
 @Table(name = "products")
 public class Product {
