fc155423cc67f2acaf1fd349ea7c2b7b320d5d1a
Luis Manuel Alegre <106556022+Manuelxx1@users.noreply.github.com>
2025-11-11 02:18:57 -0300

    Create CartItem.java

---
+++ src/main/java/com/abml/jpa/hibernate/model/CartItem.java
@@ -0,0 +1,19 @@
+@Entity
+@Table(name = "cart_items")
+public class CartItem {
+  @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @ManyToOne
+  @JoinColumn(name = "user_id")
+  private User user;
+
+  @ManyToOne
+  @JoinColumn(name = "product_id")
+  private Product product;
+
+  private Integer quantity;
+
+  @Column(name = "added_at")
+  private LocalDateTime addedAt;
+}
