ac9ad2bdbef7414a65d3333b097466028a3fa4ff
Luis Manuel Alegre <106556022+Manuelxx1@users.noreply.github.com>
2025-11-11 06:21:09 -0300

    Update Product.java

--- src/main/java/com/abml/jpa/hibernate/model/Product.java
+++ src/main/java/com/abml/jpa/hibernate/model/Product.java
@@ -1,5 +1,11 @@
 
 package com.abml.jpa.hibernate.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+
 @Entity
 @Table(name = "products")
 public class Product {
