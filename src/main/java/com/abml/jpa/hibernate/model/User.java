75eb3548f3fe1f153157d38a630a8d224b1a64db
Luis Manuel Alegre <106556022+Manuelxx1@users.noreply.github.com>
2025-11-11 02:17:00 -0300

    Create User.java

---
+++ src/main/java/com/abml/jpa/hibernate/model/User.java
@@ -0,0 +1,13 @@
+@Entity
+@Table(name = "users")
+public class User {
+  @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  private String username;
+  private String password;
+  private String email;
+
+  @Column(name = "created_at")
+  private LocalDateTime createdAt;
+}
