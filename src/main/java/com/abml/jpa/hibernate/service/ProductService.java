5e45e87d0dee61302aaf7bd3ea213f356843f867
Luis Manuel Alegre <106556022+Manuelxx1@users.noreply.github.com>
2025-11-11 04:41:54 -0300

    Create ProductService.java

---
+++ src/main/java/com/abml/jpa/hibernate/service/ProductService.java
@@ -0,0 +1,14 @@
+@Service
+public class ProductService {
+
+  @Autowired private ProductRepository productRepo;
+
+  public List<Product> getAllProducts() {
+    return productRepo.findAll();
+  }
+
+  public Product getProductById(Long id) {
+    return productRepo.findById(id)
+      .orElseThrow(() -> new RuntimeException("Producto no encontrado"));
+  }
+}
