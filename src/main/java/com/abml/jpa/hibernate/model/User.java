3ce584a0590c7d94b51784791e5fe15757681f79
Luis Manuel Alegre <106556022+Manuelxx1@users.noreply.github.com>
2025-11-11 06:37:37 -0300

    Update User.java

--- src/main/java/com/abml/jpa/hibernate/model/User.java
+++ src/main/java/com/abml/jpa/hibernate/model/User.java
@@ -10,6 +10,10 @@ import jakarta.persistence.ManyToOne;
 import jakarta.persistence.JoinColumn;
 
 
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
+
 @Entity
 @Table(name = "users")
 public class User {
