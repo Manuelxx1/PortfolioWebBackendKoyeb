f3b5fc4eee41fbd9e6fb4ef226e76d032e409e08
Luis Manuel Alegre <106556022+Manuelxx1@users.noreply.github.com>
2025-11-11 04:25:27 -0300

    Update CartItemRepository.java

--- src/main/java/com/abml/jpa/hibernate/repository/CartItemRepository.java
+++ src/main/java/com/abml/jpa/hibernate/repository/CartItemRepository.java
@@ -9,6 +9,7 @@ package com.abml.jpa.hibernate.repository;
  */
 
 import com.abml.jpa.hibernate.model.CartItem;
+import com.abml.jpa.hibernate.model.User;
 import java.util.List;
 import java.util.Optional;
 //class JpaRepository (que maneja repositorios JPA
