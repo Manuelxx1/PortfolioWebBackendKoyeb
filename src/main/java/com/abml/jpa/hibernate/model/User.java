05db976dfd7bf8dca80c709a9c4b59108ec1d443
Luis Manuel Alegre <106556022+Manuelxx1@users.noreply.github.com>
2025-11-11 06:20:43 -0300

    Update User.java

--- src/main/java/com/abml/jpa/hibernate/model/User.java
+++ src/main/java/com/abml/jpa/hibernate/model/User.java
@@ -1,5 +1,10 @@
 package com.abml.jpa.hibernate.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+
 @Entity
 @Table(name = "users")
 public class User {
