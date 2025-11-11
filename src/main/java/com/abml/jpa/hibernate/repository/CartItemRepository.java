f8b27d14e54d0b694c6eebb93f0f303589e391d4
Luis Manuel Alegre <106556022+Manuelxx1@users.noreply.github.com>
2025-11-11 02:36:42 -0300

    Update CartItemRepository.java

--- src/main/java/com/abml/jpa/hibernate/repository/CartItemRepository.java
+++ src/main/java/com/abml/jpa/hibernate/repository/CartItemRepository.java
@@ -1,3 +1,33 @@
+
+
+
+package com.abml.jpa.hibernate.repository;
+
+/**
+ *
+ * @author Flash
+ */
+
+import com.abml.jpa.hibernate.model.CartItem;
+import java.util.List;
+import java.util.Optional;
+//class JpaRepository (que maneja repositorios JPA
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
+
+
+
+/**
+ *
+ * @author Flash
+ */
+//hacemos un mapping con @Repository
+@Repository 
+@Component
 public interface CartItemRepository extends JpaRepository<CartItem, Long> {
   List<CartItem> findByUser(User user);
 }
