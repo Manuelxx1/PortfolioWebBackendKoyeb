18619f18c8891a4d8dbc201937cc6c2b3d783fba
Luis Manuel Alegre <106556022+Manuelxx1@users.noreply.github.com>
2025-11-11 06:21:50 -0300

    Update CartItem.java

--- src/main/java/com/abml/jpa/hibernate/model/CartItem.java
+++ src/main/java/com/abml/jpa/hibernate/model/CartItem.java
@@ -1,4 +1,8 @@
 package com.abml.jpa.hibernate.model;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 
 @Entity
 @Table(name = "cart_items")
