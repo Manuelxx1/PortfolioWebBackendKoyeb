1d206d417ea703c3625d41feddf6df591db6ed55
Luis Manuel Alegre <106556022+Manuelxx1@users.noreply.github.com>
2025-11-14 07:09:04 -0300

    Update PaymentController.java

--- src/main/java/com/ejercicioabml/abmlcontroller/PaymentController.java
+++ src/main/java/com/ejercicioabml/abmlcontroller/PaymentController.java
@@ -1,22 +1,19 @@
 package com.ejercicioabml.abmlcontroller;
-import com.abml.jpa.hibernate.model.Product;
+
 import com.mercadopago.MercadoPago;
 import com.mercadopago.resources.Preference;
 import com.mercadopago.resources.datastructures.preference.Item;
 import org.springframework.web.bind.annotation.*;
 
+import java.math.BigDecimal;
+
 @RestController
-
-    //@CrossOrigin es fundamental para conectar angular con el backend Springboot
-@CrossOrigin(origins = "https://4200-cs-582739288523-default.cs-us-east1-yeah.cloudshell.dev")
-
 @RequestMapping("/api/payments")
 public class PaymentController {
 
     public PaymentController() {
         // Inicializá el SDK con tu Access Token de prueba (sandbox)
-        // Token de prueba (sandbox)
-        MercadoPago.SDK.setAccessToken("APP_USR-4456023071312309-111404-da075421e24ad80c6ba26beb86c2e77a-2989163784");
+        MercadoPago.SDK.setAccessToken("TEST-xxxxxxxxxxxxxxxxxxxxxxxxxxxx");
     }
 
     // Endpoint para crear la preferencia de pago
@@ -27,7 +24,7 @@ public class PaymentController {
         Item item = new Item();
         item.setTitle(product.getName())
             .setQuantity(1)
-            .setUnitPrice((float) product.getPrice());
+            .setUnitPrice(BigDecimal.valueOf(product.getPrice())); // ✅ ahora usa BigDecimal
 
         preference.appendItem(item);
         preference.save();
