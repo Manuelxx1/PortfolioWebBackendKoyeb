a6bb735f9d2238c47c1c6b7e3739f8fcb416c928
Luis Manuel Alegre <106556022+Manuelxx1@users.noreply.github.com>
2025-11-20 08:39:45 -0300

    Create UserRepository.java

---
+++ src/main/java/com/abml/jpa/hibernate/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.abml.jpa.hibernate.repository;
+
+import com.abml.jpa.hibernate.model.Users;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<Users, Long> {
+    // método extra para buscar por mpUserId
+    Optional<Users> findByMpUserId(Long mpUserId);
+}
