55e912c681f10b77ceae82cd006ae97310ee811e
Luis Manuel Alegre <106556022+Manuelxx1@users.noreply.github.com>
2025-11-20 08:29:30 -0300

    Create Users.java

---
+++ src/main/java/com/abml/jpa/hibernate/model/Users.java
@@ -0,0 +1,42 @@
+package com.abml.jpa.hibernate.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Table;
+import jakarta.persistence.Column;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.JoinColumn;
+
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+@Table(name = "users")
+public class Users {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id; // este es el ID interno de tu sistema
+
+    private String name;
+    private String email;
+
+    @Column(name = "mp_user_id", unique = true)
+    private Long mpUserId; //  ID del usuario en Mercado Pago
+
+    // Getters y setters
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+
+    public Long getMpUserId() { return mpUserId; }
+    public void setMpUserId(Long mpUserId) { this.mpUserId = mpUserId; }
+}
