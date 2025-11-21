464c02b168e003debd7cc67785c3fc28e2745e66
Luis Manuel Alegre <106556022+Manuelxx1@users.noreply.github.com>
2025-11-21 08:11:36 -0300

    Update Users.java

--- src/main/java/com/abml/jpa/hibernate/model/Users.java
+++ src/main/java/com/abml/jpa/hibernate/model/Users.java
@@ -57,7 +57,7 @@ public class Users {
     public void setMpUserId(Long mpUserId) { this.mpUserId = mpUserId; }
     public String getUsername() { return username; }
     public void setUsername(String username) { this.username = username; }
-    public String getPassword { return password; }
+    public String getPassword() { return password; }
     public void setPassword(String password) { this.password = password; }
 
    // public List<Orders> getOrders() { return orders; }
