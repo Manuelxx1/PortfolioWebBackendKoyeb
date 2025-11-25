f68945afd2f15012c16b96718613cb4121f4b927
Luis Manuel Alegre <106556022+Manuelxx1@users.noreply.github.com>
2025-11-25 01:35:09 -0300

    Update PaymentController.java

--- src/main/java/com/ejercicioabml/abmlcontroller/PaymentController.java
+++ src/main/java/com/ejercicioabml/abmlcontroller/PaymentController.java
@@ -65,7 +65,7 @@ public ResponseEntity<String> createPreference(@PathVariable Long productId) {
         PreferenceRequest request = PreferenceRequest.builder()
                 .items(Arrays.asList(item))
                 .notificationUrl("https://portfoliowebbackendkoyeb-1.onrender.com/api/payments/webhook")
-            .metadata(Map.of("preference_id", "tuPreferenceId"))   
+            .metadata(Map.of("preference_id", productId.toString())) 
                 .build();
 
         Preference preference = preferenceClient.create(request);
