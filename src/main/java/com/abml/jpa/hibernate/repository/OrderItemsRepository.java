379791a0904747a500189f5087230f59144a621c
Luis Manuel Alegre <106556022+Manuelxx1@users.noreply.github.com>
2025-11-25 06:41:53 -0300

    Update OrderItemsRepository.java

--- src/main/java/com/abml/jpa/hibernate/repository/OrderItemsRepository.java
+++ src/main/java/com/abml/jpa/hibernate/repository/OrderItemsRepository.java
@@ -1,4 +1,5 @@
 package com.abml.jpa.hibernate.repository;
+import com.abml.jpa.hibernate.model.OrderItems;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
