244ed345f27785690e0d8b7a8f5ce024c9a52d23
Luis Manuel Alegre <106556022+Manuelxx1@users.noreply.github.com>
2025-11-24 23:46:39 -0300

    Update OrderRepository.java

--- src/main/java/com/abml/jpa/hibernate/repository/OrderRepository.java
+++ src/main/java/com/abml/jpa/hibernate/repository/OrderRepository.java
@@ -4,7 +4,7 @@ import com.abml.jpa.hibernate.model.Orders;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface OrderRepository extends JpaRepository<Orders, Long> {
-
+//Agrega un método custom
     // Buscar orden por preferenceId
     Optional<Orders> findByPreferenceId(String preferenceId);
 }
