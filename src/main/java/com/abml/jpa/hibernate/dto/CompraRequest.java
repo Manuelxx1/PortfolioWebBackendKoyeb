70e23f7c3f6b90d0442c0b43b86ebe79bbfb1178
Luis Manuel Alegre <106556022+Manuelxx1@users.noreply.github.com>
2025-12-02 06:14:36 -0300

    Create CompraRequest.java

---
+++ src/main/java/com/abml/jpa/hibernate/dto/CompraRequest.java
@@ -0,0 +1,23 @@
+// CompraRequest.java
+//este dto reemplaza a Map<String, Object> 
+//en el endpoint create del checkout mp para tener mejor control y acceso
+//de los datos que llegan al endpoint 
+public class CompraRequest {
+    private int quantity;
+    private String usuario;
+
+    // Getters y setters
+    public int getQuantity() {
+        return quantity;
+    }
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+}
