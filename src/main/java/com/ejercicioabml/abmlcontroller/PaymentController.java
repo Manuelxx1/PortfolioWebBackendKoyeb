ee897b038ddd602ac9c36a8a0d5eba00e045e499
Luis Manuel Alegre <106556022+Manuelxx1@users.noreply.github.com>
2025-12-03 01:14:35 -0300

    Update PaymentController.java

--- src/main/java/com/ejercicioabml/abmlcontroller/PaymentController.java
+++ src/main/java/com/ejercicioabml/abmlcontroller/PaymentController.java
@@ -83,10 +83,9 @@ public ResponseEntity<String> createPreference(
 
         // Crear ítem para la preferencia
         Item item = new Item();
-        item.setTitle(product.getName() + "x" + quantity)"
-
-            .setQuantity(quantity)
-            .setUnitPrice(product.getPrice().doubleValue());
+item.setTitle(product.getName() + " (x" + quantity)");
+item.setQuantity(quantity);
+item.setUnitPrice(product.getPrice().doubleValue());
 
         // Crear preferencia
         Preference preference = new Preference();
