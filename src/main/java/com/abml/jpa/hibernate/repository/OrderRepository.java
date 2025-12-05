640745c15d6986201842ff18ffccdcf931f3dcbc
Luis Manuel Alegre <106556022+Manuelxx1@users.noreply.github.com>
2025-12-05 07:52:42 -0300

    Update OrderRepository.java

--- src/main/java/com/abml/jpa/hibernate/repository/OrderRepository.java
+++ src/main/java/com/abml/jpa/hibernate/repository/OrderRepository.java
@@ -2,6 +2,8 @@ package com.abml.jpa.hibernate.repository;
 
 import com.abml.jpa.hibernate.model.Orders;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
 import java.util.Optional;
 
 public interface OrderRepository extends JpaRepository<Orders, Long> {
