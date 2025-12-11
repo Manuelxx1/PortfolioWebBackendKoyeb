42c36973576da2ccf8e88ca7ddcf19c5d9dba229
Luis Manuel Alegre <106556022+Manuelxx1@users.noreply.github.com>
2025-12-11 06:46:49 -0300

    Update ProductService.java

--- src/main/java/com/abml/jpa/hibernate/service/ProductService.java
+++ src/main/java/com/abml/jpa/hibernate/service/ProductService.java
@@ -1,7 +1,8 @@
 package com.abml.jpa.hibernate.service;
 import com.abml.jpa.hibernate.model.Product;
 import com.abml.jpa.hibernate.repository.ProductRepository;
-
+import com.abml.jpa.hibernate.repository.UserRepository;
+import com.abml.jpa.hibernate.model.Users;
 import org.springframework.stereotype.Service;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -11,6 +12,7 @@ import java.util.List;
 public class ProductService {
 
   @Autowired private ProductRepository productRepo;
+    @Autowired private UserRepository userRepository;
 
   public List<Product> searchByName(String name) {
     return productRepo.findByNameContainingIgnoreCase(name);
@@ -21,4 +23,14 @@ public class ProductService {
     return productRepo.findById(id)
       .orElseThrow(() -> new RuntimeException("Producto no encontrado"));
   }
+
+  
+public void updatePassword(String usuario, String nuevaPassword) {
+        Users user = userRepository.findByUsuario(usuario)
+                .orElseThrow(() -> new RuntimeException("Usuario no encontrado"));
+
+        user.setPassword(nuevaPassword); // acá deberías encriptar la contraseña
+        userRepository.save(user);
+}
+  
 }
