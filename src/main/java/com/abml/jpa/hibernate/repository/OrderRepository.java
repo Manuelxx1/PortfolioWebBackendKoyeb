5513c08b27c29b1075d31b5e62115e48866c48ee
Luis Manuel Alegre <106556022+Manuelxx1@users.noreply.github.com>
2025-12-16 05:31:00 -0300

    Update OrderRepository.java

--- src/main/java/com/abml/jpa/hibernate/repository/OrderRepository.java
+++ src/main/java/com/abml/jpa/hibernate/repository/OrderRepository.java
@@ -12,4 +12,9 @@ public interface OrderRepository extends JpaRepository<Orders, Long> {
     Optional<Orders> findByPreferenceId(String preferenceId);
     List<Orders> findByLoginUsername(String loginUsername);
 
+
+    List<Orders> findByUserId(Long userId);
+
+    // Alternativa: si Orders tiene una relación ManyToOne con Users
+    List<Orders> findByUser(Users user);
 }
