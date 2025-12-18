c40b63f841f144ddbd7fa54d2eee6ea201f61398
Luis Manuel Alegre <106556022+Manuelxx1@users.noreply.github.com>
2025-12-18 01:49:07 -0300

    Update PaymentController.java

--- src/main/java/com/ejercicioabml/abmlcontroller/PaymentController.java
+++ src/main/java/com/ejercicioabml/abmlcontroller/PaymentController.java
@@ -83,7 +83,7 @@ public class PaymentController {
         Users usuario = null;
         if (compraRequestDTO != null && compraRequestDTO.getIdUsuario() != null) {
             usuario = userRepository.findById(compraRequestDTO.getIdUsuario()).orElse(null);
-            
+            System.out.println(usuario);
                 }
 
         if (usuario == null) {
@@ -154,7 +154,7 @@ public class PaymentController {
             orderRepository.save(order);
 
             // 10. Retornar InitPoint
-            return ResponseEntity.ok(usuario + preference.getInitPoint());
+            return ResponseEntity.ok(preference.getInitPoint());
 
         } catch (Exception e) {
             log.error("Error creando preferencia para productId: " + productId, e);
