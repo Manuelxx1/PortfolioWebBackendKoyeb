33df432aea8d1c2c52014fbc1691466624d3bc5f
Luis Manuel Alegre <106556022+Manuelxx1@users.noreply.github.com>
2025-12-20 06:42:16 -0300

    Update WebSocketConfig.java

--- src/main/java/com/ejercicioabml/abmlcontroller/config/WebSocketConfig.java
+++ src/main/java/com/ejercicioabml/abmlcontroller/config/WebSocketConfig.java
@@ -1,5 +1,12 @@
 package com.ejercicioabml.abmlcontroller.config;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+
+
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
