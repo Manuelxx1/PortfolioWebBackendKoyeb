749633bc169b054ad33626784c4c64c431c659d0
Luis Manuel Alegre <106556022+Manuelxx1@users.noreply.github.com>
2025-12-20 06:14:49 -0300

    Update WebSocketConfig.java

--- src/main/java/com/ejercicioabml/abmlcontroller/config/WebSocketConfig.java
+++ src/main/java/com/ejercicioabml/abmlcontroller/config/WebSocketConfig.java
@@ -4,12 +4,15 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
+        // Endpoint al que se conecta Angular
         registry.addEndpoint("/ws").setAllowedOrigins("*").withSockJS();
     }
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
+        // Prefijo para los mensajes que envía el servidor
         registry.enableSimpleBroker("/topic");
+        // Prefijo para los mensajes que envía el cliente
         registry.setApplicationDestinationPrefixes("/app");
     }
 }
