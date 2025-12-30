57af4d5e4843e24b3d6582cf91cdab62b083e046
Luis Manuel Alegre <106556022+Manuelxx1@users.noreply.github.com>
2025-12-30 05:51:59 -0300

    Update AbmlApplication.java

--- src/main/java/com/ejercicioabml/abmlcontroller/AbmlApplication.java
+++ src/main/java/com/ejercicioabml/abmlcontroller/AbmlApplication.java
@@ -5,8 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+    @EnableScheduling
 @EnableJpaRepositories(basePackages = "com.abml.jpa.hibernate.repository")
 @EntityScan(basePackages = "com.abml.jpa.hibernate.model")
 @ComponentScan(basePackages = {
