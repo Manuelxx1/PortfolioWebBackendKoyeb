5bbf8952819a7a5b9bfbca95ef87d81949320575
Luis Manuel Alegre <106556022+Manuelxx1@users.noreply.github.com>
2025-12-30 06:09:48 -0300

    Create NotificacionAutomatica.java

---
+++ src/main/java/com/abml/jpa/hibernate/scheduler/NotificacionAutomatica.java
@@ -0,0 +1,18 @@
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+public class NotificacionAutomatica {
+
+    @Autowired
+    private SimpMessagingTemplate template;
+
+    // Enviar cada 10 segundos
+    @Scheduled(fixedRate = 10000)
+    public void enviarAutomatico() {
+        String mensaje = "🔔 Notificación automática desde el servidor: " + System.currentTimeMillis();
+        template.convertAndSend("/topic/notificaciones", mensaje);
+    }
+}
