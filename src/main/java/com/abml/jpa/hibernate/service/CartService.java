cc5d1a66db73d3f24ac95e75cf084404d8d8e8d2
Luis Manuel Alegre <106556022+Manuelxx1@users.noreply.github.com>
2026-01-03 06:28:01 -0300

    Update CartService.java

--- src/main/java/com/abml/jpa/hibernate/service/CartService.java
+++ src/main/java/com/abml/jpa/hibernate/service/CartService.java
@@ -97,7 +97,7 @@ public void decreaseFromCart(Users user, Long productId) {
   public void enviarRecordatorios() { 
     // Carritos con items agregados hace más de 24 horas
     LocalDateTime limite = LocalDateTime.now().minusHours(24);
-    List<CartItem> abandonados = cartRepo.findAbandonedCarts(limite); 
+    List<CartItem> abandonados = cartRepo.findByAddedAtBefore(limite); 
     for (CartItem item : abandonados) { 
       String mensaje = "⏰ Recordatorio: aún tienes " + item.getProduct().getName() + " en tu carrito";
       template.convertAndSend("/topic/notificaciones", mensaje); 
