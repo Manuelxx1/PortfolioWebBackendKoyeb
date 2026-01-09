15a96a33b52b7171fec161246941bde26506e278
Luis Manuel Alegre <106556022+Manuelxx1@users.noreply.github.com>
2026-01-09 07:16:12 -0300

    Update PaymentController.java

--- src/main/java/com/ejercicioabml/abmlcontroller/PaymentController.java
+++ src/main/java/com/ejercicioabml/abmlcontroller/PaymentController.java
@@ -120,7 +120,7 @@ System.out.println("Usuario encontrado: " + usuario);
             // 6. Crear la Preference Request usando el Builder
 // 1. Crear el objeto de Back URLs por separado
 PreferenceBackUrlsRequest backUrls = PreferenceBackUrlsRequest.builder()
-    .success("https://4200-cs-582739288523-default.cs-us-east1-yeah.cloudshell.dev")
+    .success("https://4200-cs-582739288523-default.cs-us-east1-yeah.cloudshell.dev/compra-exitosa")
     .failure("https://tusitio.com/compra-fallida")
     .pending("https://tusitio.com/compra-pendiente")
     .build();
