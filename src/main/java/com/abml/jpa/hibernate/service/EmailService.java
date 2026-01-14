1e48b87f4c7a287b37485ee4e6578276bf7d082d
Luis Manuel Alegre <106556022+Manuelxx1@users.noreply.github.com>
2026-01-14 07:58:05 -0300

    Update EmailService.java

--- src/main/java/com/abml/jpa/hibernate/service/EmailService.java
+++ src/main/java/com/abml/jpa/hibernate/service/EmailService.java
@@ -1,3 +1,4 @@
+package com.abml.jpa.hibernate.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
