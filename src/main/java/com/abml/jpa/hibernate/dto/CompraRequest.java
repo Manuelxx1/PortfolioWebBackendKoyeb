a43d6d9a11d1ed6f0f2053e9590e1febb463e22e
Luis Manuel Alegre <106556022+Manuelxx1@users.noreply.github.com>
2026-01-27 07:45:18 -0300

    Refactor CompraRequest class and add new fields

--- src/main/java/com/abml/jpa/hibernate/dto/CompraRequest.java
+++ src/main/java/com/abml/jpa/hibernate/dto/CompraRequest.java
@@ -9,7 +9,19 @@ public class CompraRequest {
     private int quantity;
     private Long idUsuario; // <-- nuevo campo
     private String usuario;
-    private String shippingType; // "standard", "express", "pickup" 
+   // private String shippingType; // "standard", "express", "pickup" 
+    //private double shippingCost;
+   //private String shippingName;
+
+    // Datos personales 
+    private String name;
+    private String email; 
+    private String phone;
+    private String address;
+    private String city; 
+    private String postalCode; 
+    // Datos de envío 
+    private String shippingType; 
     private double shippingCost;
     private String shippingName;
 
@@ -36,31 +48,60 @@ public class CompraRequest {
 
     
 
+    public String getName() { 
+        return name; 
+    }
+    public void setName(String name) {
+        this.name = name; 
+    } 
+    public String getEmail() { 
+        return email; 
+    } 
+    public void setEmail(String email) { 
+        this.email = email; 
+    }
+    public String getPhone() { 
+        return phone; 
+    } 
+    public void setPhone(String phone) {
+        this.phone = phone; 
+    } 
+    public String getAddress() { 
+        return address;
+    }
+    public void setAddress(String address) {
+        this.address = address; 
+    } 
+    public String getCity() {
+        return city; 
+    } 
+    public void setCity(String city) { 
+        this.city = city; 
+    } 
+    public String getPostalCode() {
+        return postalCode;
+    } 
+    public void setPostalCode(String postalCode) { 
+        this.postalCode = postalCode; 
+    }
     public String getShippingType() {
         return shippingType;
-    }
-    public void setShippingType(String shippingType) {
+    } 
+    public void setShippingType(String shippingType) { 
         this.shippingType = shippingType;
     }
-
     public double getShippingCost() {
         return shippingCost;
-    }
-    public void setShippingCost(double shippingCost) {
+    } 
+    public void setShippingCost(double shippingCost) { 
         this.shippingCost = shippingCost;
-    }
-
-
-
-    
-
-public String getShippingName() {
-    return shippingName;
-}
-
-public void setShippingName(String shippingName) {
-    this.shippingName = shippingName;
-}
+    } 
+    public String getShippingName() {
+        return shippingName;
+    } 
+    public void setShippingName(String shippingName) { 
+        this.shippingName = shippingName;
+    } 
 
 
         @Override
