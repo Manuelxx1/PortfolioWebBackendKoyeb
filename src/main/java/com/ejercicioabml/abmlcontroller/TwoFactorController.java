baf91f92bb96be4344027c026c5c0e83af110120
Luis Manuel Alegre <106556022+Manuelxx1@users.noreply.github.com>
2026-03-07 06:00:41 -0300

    Update TwoFactorController.java

--- src/main/java/com/ejercicioabml/abmlcontroller/TwoFactorController.java
+++ src/main/java/com/ejercicioabml/abmlcontroller/TwoFactorController.java
@@ -1,3 +1,4 @@
+package com.ejercicioabml.abmlcontroller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
