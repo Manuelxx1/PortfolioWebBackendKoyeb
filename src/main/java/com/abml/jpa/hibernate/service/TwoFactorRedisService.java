bfc33480cb3a6424717697df4147fa84e4627178
Luis Manuel Alegre <106556022+Manuelxx1@users.noreply.github.com>
2026-03-07 06:04:12 -0300

    Update TwoFactorRedisService.java

--- src/main/java/com/abml/jpa/hibernate/service/TwoFactorRedisService.java
+++ src/main/java/com/abml/jpa/hibernate/service/TwoFactorRedisService.java
@@ -1,3 +1,5 @@
+
+package com.abml.jpa.hibernate.service;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 import java.util.concurrent.TimeUnit;
