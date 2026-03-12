1864d4801544e21c2396fd58c5e3d6619139a9e8
Luis Manuel Alegre <106556022+Manuelxx1@users.noreply.github.com>
2026-03-12 00:53:10 -0300

    Update TwoFactorController.java

--- src/main/java/com/ejercicioabml/abmlcontroller/TwoFactorController.java
+++ src/main/java/com/ejercicioabml/abmlcontroller/TwoFactorController.java
@@ -32,7 +32,7 @@ private String generateCode() {
       
         
         // Llamada al microservicio en Termux vía túnel
-    String url = "https://throws-release-combat-patient.trycloudflare.com/api/send?email=" + email + "&code=" + code;
+    String url = "https://investment-huge-backing-shaw.trycloudflare.com/api/send?email=" + email + "&code=" + code;
         RestTemplate restTemplate = new RestTemplate();
     restTemplate.postForObject(url, null, String.class);
         return ResponseEntity.ok("Código enviado a " + email);
