9b3446c75cd2f8af705d2e3934589b0cf3fe0584
Luis Manuel Alegre <106556022+Manuelxx1@users.noreply.github.com>
2026-03-13 05:41:37 -0300

    Update TestPassword.java

--- src/main/java/com/abml/jpa/hibernate/service/TestPassword.java
+++ src/main/java/com/abml/jpa/hibernate/service/TestPassword.java
@@ -3,7 +3,11 @@ package com.abml.jpa.hibernate.service;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class TestPassword {
+    private static final Logger log = LoggerFactory.getLogger(LoginController.class);
     public static void main(String[] args) {
         PasswordEncoder encoder = new BCryptPasswordEncoder();
 
@@ -11,7 +15,7 @@ public class TestPassword {
         String hashedPassword = "$2a$10$TrzYiTOSXDf7QChkNt9H2u.rAatsBgWJHn3XCmiD.B6nfvwnhuDMS";
 
         boolean matches = encoder.matches(rawPassword, hashedPassword);
-
+log.info("Datos del LoginDTO TestPassword recibido: username={} password={}", dto.getUsername(), dto.getPassword());
         System.out.println("¿Coincide? " + matches);
     }
 }
