78fd41bffcc67ab2b968133462b1816fb2060965
Luis Manuel Alegre <106556022+Manuelxx1@users.noreply.github.com>
2026-05-13 01:45:37 -0300

    Add case-insensitive search methods for sections and categories

--- src/main/java/com/abml/jpa/hibernate/repository/ProductRepository.java
+++ src/main/java/com/abml/jpa/hibernate/repository/ProductRepository.java
@@ -37,12 +37,13 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
   //Revisar este metodo o modificarlo para que funcione otra vez
   //List<Product> findBySection(Section section);
 
-  List<Product> findBySection_Name(String sectionName);
+  List<Product> findBySection_NameIgnoreCase(String sectionName);
+
 
   //mostrar Productos por categoría 
 //Si querés filtrar por el nombre de la categoría (que es un String dentro de la entidad Category)
   // Esto le dice a Spring Data JPA que use la propiedad name de la entidad relacionada Category.
-  List<Product> findByCategory_Name(String categoryName);
+  List<Product> findByCategory_NameIgnoreCase(String categoryName);
 
   //para el backoffice filtro para realizar consultas dinamicas 
 @Query("SELECT p FROM Product p WHERE " +
