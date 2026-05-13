474098a6b518f1a30d674be303f636c3398759ed
Luis Manuel Alegre <106556022+Manuelxx1@users.noreply.github.com>
2026-05-13 00:54:26 -0300

    Create SectionRepository interface for Section entity

---
+++ src/main/java/com/abml/jpa/hibernate/repository/SectionRepository.java
@@ -0,0 +1,22 @@
+package com.abml.jpa.hibernate.repository;
+
+
+import com.abml.jpa.hibernate.model.Product;
+import com.abml.jpa.hibernate.model.Section;
+import java.util.List;
+import java.util.Optional;
+//class JpaRepository (que maneja repositorios JPA
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
+
+
+//hacemos un mapping con @Repository
+@Repository 
+@Component
+public interface SectionRepository extends JpaRepository<Section, Long> {
+    Section findByName(String name);
+}
