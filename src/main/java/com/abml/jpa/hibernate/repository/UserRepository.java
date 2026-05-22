696ba61ec7114f7912e76ed09dc306395a25ab9c
Luis Manuel Alegre <106556022+Manuelxx1@users.noreply.github.com>
2026-05-22 10:15:29 -0300

    Update UserRepository.java

--- src/main/java/com/abml/jpa/hibernate/repository/UserRepository.java
+++ src/main/java/com/abml/jpa/hibernate/repository/UserRepository.java
@@ -10,6 +10,8 @@ public interface UserRepository extends JpaRepository<Users, Long> {
 Optional<Users> findByUsername(String username);
     Optional<Users> findByEmail(String email); //  este faltaba
 Optional<Users> findById(Long id);
+    Optional<Users> findByPasswordResetToken(String token);
+
  
 
 
